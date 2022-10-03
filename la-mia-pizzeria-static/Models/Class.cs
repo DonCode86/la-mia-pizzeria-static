4e8ed6d9116fd81bf33ced203c5a4a0f1c56f344
DonCode <99022919+DonCode86@users.noreply.github.com>
2022-10-03 15:34:00 +0200

    Add project files.

---
+++ la-mia-pizzeria-static/Models/Class.cs
@@ -0,0 +1,19 @@
+﻿namespace la_mia_pizzeria_static.Models
+{
+    public class Pizza
+    {
+        public Pizza(string name, string ingredients, string image, double price)
+        {
+            Name = name;
+            Ingredients = ingredients;
+            Image = image;
+            Price = price;
+        }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Ingredients { get; set; }
+        public string Image { get; set; }
+        public double Price { get; set; }
+
+    }
+}
