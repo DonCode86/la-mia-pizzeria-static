2c562c8946cdece1bea6665e81a0421cf082dabf
DonCode <99022919+DonCode86@users.noreply.github.com>
2022-10-03 16:19:18 +0200

    add-exercises-link

---
+++ la-mia-pizzeria-static/ExercisesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace la_mia_pizzeria_static
+{
+    public class ExercisesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
